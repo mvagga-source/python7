d8b8b5799f943b4c3fc331caeb226babd83bb650
m.vagga <m.vagga@gmail.com>
2025-11-04 21:29:00 +0900

    클래스

--- p1104/p10_클래스.py
+++ p1104/p10_클래스.py
@@ -24,14 +24,52 @@ class Stu:
     def rank_f(self):
         pass
     
-    def add_f(self, s):
+    # def add_f(self, s):
+    #     self.stu_list.append(
+    #         {'stuno':s.stuno,'name':s.name,'kor':s.kor,'eng':s.eng,'math':s.math,'total':s.total,'avg':s.avg,"rank":s.rank}
+    #     )
+
+
+class Stuadd:
+    
+    stu_list = []
+    titles = ['번호','이름','국어','영어','수학','합계','평균','등수']
+        
+    def add_f(self,s):
         self.stu_list.append(
-            {'stuno':s.stuno,'name':s.name,'kor':s.kor,'eng':s.eng,'math':s.math,'total':s.total,'avg':s.avg,"rank":s.rank}
-        )
+            {'stuno':s.stuno,'name':s.name,'kor':s.kor,'eng':s.eng,'math':s.math,'total':s.total,'avg':s.avg,'rank':s.rank}
+        )   
+        
+        
+    def rank_f(self):
+
+        for i in self.stu_list:
+            c = 1
+            for j in self.stu_list:
+                
+                if i['total'] < j['total']:
+                    c += 1
+            i['rank'] = c        
+            
+    def print_f(self):
+        
+        for i in self.stu_list:
+            print(f"{i['stuno']}\t{i['name']}\t{i['kor']}\t{i['eng']}\t{i['math']}\t{i['total']}\t{i['avg']:.2f}\t{i['rank']}")
+
+
     
 s1 = Stu(10101,"홍길동",100,100,100)
-s1.add_f(s1)
-print(s1.stu_list)
+s2 = Stuadd()
+s2.add_f(Stu(10101,"홍길동",100,100,100))
+s2.add_f(Stu(10101,"고길동",80,70,10))
+s2.add_f(Stu(10101,"이길동",100,90,20))
+print(s2.stu_list)
+s2.rank_f()
+print(s2.stu_list)
+s2.print_f()
+
+
+
         
     
 
